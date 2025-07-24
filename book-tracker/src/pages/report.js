f9d5c25b12b428e29605295ef8b4873beed6c117
Leanne <lkeeley@pace.edu>
2025-07-24 16:57:00 -0400

    added visualizations for yearly report

--- book-tracker/src/pages/report.js
+++ book-tracker/src/pages/report.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import YearForm from '../components/yearForm';
+import GaugeChart from 'react-gauge-chart'
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 function Report() {
     const [ reportDetails, setReportDetails ] = useState(null);
     const [ year, setYear ] = useState(new Date().getFullYear());
+    const [ data, setData ] = useState({});
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/reports/yearly?year=${encodeURIComponent(year)}`)
@@ -11,6 +14,22 @@ function Report() {
             .then(setReportDetails)
             .catch(err => console.error(err));
     }, [year]);
+
+    useEffect(() => {
+        if (!reportDetails) return;
+        setData({
+            longestShortest: [
+                {
+                    name: 'longest',
+                    pgCount: reportDetails.longest[0].maxPages.pageCount
+                },
+                {
+                    name: 'shortest',
+                    pgCount: reportDetails.shortest[0].minPages.pageCount
+                }
+            ]
+        });
+    }, [reportDetails])
 
     const handleChange = (e) => {
         e.preventDefault();
@@ -22,16 +41,62 @@ function Report() {
     return (
         <>
         <YearForm handleChange={handleChange}/>
-        <div class="d-flex flex-row justify-content-around">
-            <div class="p-3"><h4>Total Books Read:</h4>
-                {reportDetails && reportDetails.bookCount.toLocaleString()}</div>
-            <div class="p-3"><h4>Total Page Count:</h4>
-                {reportDetails && reportDetails.pageCount[0].totalPageCount.toLocaleString()}</div>
-
-            <div class="p-3"><h3>Longest Book</h3>{reportDetails.longest[0].maxPages.title}<br/>
-                Pages: {reportDetails.longest[0].maxPages.pageCount.toLocaleString()}</div>
-            <div class="p-3"><h3>Shortest Book</h3>{reportDetails.shortest[0].minPages.title}<br/>
-                Pages: {reportDetails.shortest[0].minPages.pageCount.toLocaleString()}</div>
+        <div className="d-flex flex-row justify-content-around">
+            <div className="p-2 m-3 grey-tile">
+                <h4 className="text-center">Total Books Read</h4>
+                <p className="text-center">{reportDetails && reportDetails.bookCount.toLocaleString()}</p>
+                {/* max books set to 80 */}
+                <GaugeChart id="gauge-chart2" 
+                    colors={["#FF5F6D", "#515affff"]} 
+                    nrOfLevels={4} 
+                    percent={reportDetails.bookCount / 80}
+                    hideText={true}
+                />
+            </div>
+            <div className="p-2 m-3 grey-tile">
+                <h4 className="text-center">Total Page Count</h4>
+                <p className="text-center">{reportDetails && reportDetails.pageCount[0].totalPageCount.toLocaleString()}</p>
+                {/* max pages set to 20,000 */}
+                <GaugeChart id="gauge-chart2" 
+                    colors={["#ffd25fff", "#51ff60ff"]} 
+                    nrOfLevels={4} 
+                    percent={reportDetails.pageCount[0].totalPageCount / 20000}
+                    hideText={true}
+                />
+            </div>
+        </div>
+        <div className="d-flex flex-row justify-content-around">
+            <div className="p-3 m-3 grey-tile">
+                <h4 className="text-center">Longest Book</h4>
+                <p className="text-center">{reportDetails.longest[0].maxPages.title}</p>
+                <p className="text-center">Pages: {reportDetails.longest[0].maxPages.pageCount.toLocaleString()}</p>
+            </div>
+            <div className="p-2 m-3 grey-tile w-50">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                    width={500}
+                    height={300}
+                    data={data.longestShortest}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="pgCount" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+            <div className="p-3 m-3 grey-tile">
+                <h4 className="text-center">Shortest Book</h4>
+                <p className="text-center">{reportDetails.shortest[0].minPages.title}</p>
+                <p className="text-center">Pages: {reportDetails.shortest[0].minPages.pageCount.toLocaleString()}</p>
+            </div>
         </div>
         </>
     );
